188bd7b254aabadd1b12d2618bf4ca85e54d1b03
chalaniChamodya <chalanichamodya2003@gmail.com>
2024-07-19 01:39:17 +0530

    final commit

---
+++ src/main/java/lk/ijse/Main.java
@@ -0,0 +1,25 @@
+package lk.ijse;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Chalani", 21);
+
+        // Serialization
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("person.ser"))) {
+            oos.writeObject(person);
+            System.out.println("Serialization successful: " + person);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Deserialization
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("person.ser"))) {
+            Person deserializedPerson = (Person) ois.readObject();
+            System.out.println("Deserialization successful: " + deserializedPerson);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
